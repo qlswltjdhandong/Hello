cb4adcbaef29ecdf82022a94ad4f70acede3c4c3
qlswltjdhandong <22100349@handong.ac.kr>
2022-09-02 10:29:40 +0900

    last

--- src/main/java/org/example/Word.java
+++ src/main/java/org/example/Word.java
@@ -7,6 +7,11 @@ public class Word {
     public Word() {
     }
 
+    public Word(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public String getId() {
         return id;
     }
