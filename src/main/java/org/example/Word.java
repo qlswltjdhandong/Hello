94869ae091d87b5f972c17f2d1452153a5cb15a7
qlswltjdhandong <22100349@handong.ac.kr>
2022-09-02 10:28:44 +0900

    first

---
+++ src/main/java/org/example/Word.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class Word {
+    String id;
+    String name;
+
+    public Word() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
